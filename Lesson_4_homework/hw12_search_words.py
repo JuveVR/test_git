d85d9c264779e5747bebb2931fd39aab2d9d8009
VadymVR <rovenkovad@gmail.com>
2024-05-03 09:17:32 +0300

    Reworked the task according to proper requirements with oo

--- Lesson_4_homework/hw12_search_words.py
+++ Lesson_4_homework/hw12_search_words.py
@@ -3,21 +3,19 @@
 # Print the all words, containing the letter 'o' (in any case (upper/lower)) twice, as a title.
 
  # option 1
-o_string = "This tool is cool. But that owl owl owl owl owl is awful. MAGIC TOOLS Ltd."
+o_string = "This tool is cool. But that owl oowl oowl owl owl is awful. MAGIC TOOLS Ltd."
 words_list = o_string.split()
 o_words = []
 for word in words_list:
-    lower_word = word.lower()
-    if lower_word.find('o') >= 0:
-        o_words.append(word)
+    if word.lower().count('o') == 2:
+        o_words.append(word.lower().title())
 print("Return:", end=" ")
 print(*o_words)
 
-
-
-# option 2 (almost the same ad 1)
+# option 2 (almost the same as 1)
 o_words = []
 for word in words_list:
-    if 'O' in word.upper():
-        o_words.append(word)
-print("Result:", " ".join(o_words))
+    if word.lower().count('o') == 2:
+        o_words.append(word.lower())
+
+print("Result:", " ".join(word.title() for word in o_words))
