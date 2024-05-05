dea3371370d0bc3303fd56316744f27029c68b25
VadymVR <rovenkovad@gmail.com>
2024-05-05 21:34:53 +0300

    Fixed comparison operators

--- Lesson_5_homework/hw_15_list_manipulation.py
+++ Lesson_5_homework/hw_15_list_manipulation.py
@@ -3,25 +3,25 @@
 # If there are no such elements, print None object for both the sum and the product.
 
 # option 1
-lst = [2, 4, 6, 2, 5, 1, 1, 9, 4, 6]
+lst = [2, 3, 2, 5, 1, 1, 9, 4, 6]
 MIN = 3
 MAX = 6
 filtered_list = []
 
 multiply_result = 1
 for i in lst:
-    if MIN < i < MAX:
+    if MIN <= i <= MAX:
         multiply_result = multiply_result * i
         filtered_list.append(i)
 
 if len(filtered_list) == 0:
     print("sum_: None object", "product: None object")
 else:
-    print(f"sum_: {sum(filtered_list)}, product: {multiply_result}" )
+    print(f"sum_: {sum(filtered_list)}, product: {multiply_result}")
 
 
 # option 2
-filtered_list = [i for i in lst if MIN < i < MAX]
+filtered_list = [i for i in lst if MIN <= i <= MAX]
 
 if not filtered_list:
     print("sum_:", None)
