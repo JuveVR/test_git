4d8f432a4af381cab8b4fa4fa5f0af0e5eee4efb
VadymVR <rovenkovad@gmail.com>
2024-05-13 02:39:34 +0300

    Possible fix for my sorting

--- Lesson_7_homework/hw_24_sorting_dates.py
+++ Lesson_7_homework/hw_24_sorting_dates.py
@@ -7,8 +7,12 @@
 # Don't use libs to work with dates
 
 
-my_dates = ['14-Dec', '12-Apr', '13-Apr', '31-Dec', '1-Jan', '12-Jan']
+my_dates = ['14-Dec', '12-Apr', '13-Apr', '31-Dec', '1-Jan', '12-Jan', '1-Jun', '1-May', '30-Feb']
 
-sorted_dates = sorted(my_dates, key=lambda x: (x.split('-')[1], int(x.split('-')[0])))
+month_mapping = {'Jan': 1, 'Feb': 2, 'Mar': 3,
+               'Apr': 4, 'May': 5, 'Jun': 6,
+               'Jul': 7, 'Aug': 8, 'Sep': 9,
+               'Oct': 10, 'Nov': 11, 'Dec': 12}
 
+sorted_dates = sorted(my_dates, key=lambda x: (month_mapping[x.split('-')[1]], int(x.split('-')[0])))
 print(sorted_dates)
